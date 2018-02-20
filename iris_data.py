f557bf1f57ecbb9683d3fde2254bc42418ae979a
boyuan <fengboyuannj@gmail.com>
2018-02-20 12:33:52 -0500

    can use customize data as input

--- iris_data.py
+++ iris_data.py
@@ -39,10 +39,41 @@ def train_input_fn(features, labels, batch_size):
     return dataset.make_one_shot_iterator().get_next()
     #return dataset
 
+def train_input_fn_dict(features, labels, batch_size):
+    """An input function for training"""
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((features, labels))
+
+    # Shuffle, repeat, and batch the examples.
+    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+
+    # Return the dataset.
+    return dataset.make_one_shot_iterator().get_next()
+    #return dataset
+
 
 def eval_input_fn(features, labels, batch_size):
     """An input function for evaluation or prediction"""
     features=dict(features)
+    if labels is None:
+        # No labels, use only features.
+        inputs = features
+    else:
+        inputs = (features, labels)
+
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices(inputs)
+
+    # Batch the examples
+    assert batch_size is not None, "batch_size must not be None"
+    dataset = dataset.batch(batch_size)
+
+    # Return the dataset.
+    return dataset.make_one_shot_iterator().get_next()
+    #return dataset
+
+def eval_input_fn_dict(features, labels, batch_size):
+    """An input function for evaluation or prediction"""
     if labels is None:
         # No labels, use only features.
         inputs = features
--- premade_estimator_sklearn.py
+++ premade_estimator_sklearn.py
@@ -16,6 +16,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from sklearn import datasets
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import StandardScaler
+from pandas import Series
 
 import argparse
 import tensorflow as tf
@@ -32,11 +36,31 @@ def main(argv):
     args = parser.parse_args(argv[1:])
 
     # method 1: using tf to Fetch the data
-    (train_x, train_y), (test_x, test_y) = iris_data.load_data()
+    #(train_x, train_y), (test_x, test_y) = iris_data.load_data()
+
+    # method 2: using sklearn to Fetch the data
+    iris = datasets.load_iris()
+    iris_data_X = iris["data"][:]
+    iris_data_y = (iris["target"][:]).astype(np.int)
+    feature_length = iris_data_X.shape[1]
+    data_num = iris_data_X.shape[0]
+    iris_data_y = iris_data_y.reshape(-1, 1)
+
+    scaler = StandardScaler()
+    scaled_iris_data = scaler.fit_transform(iris_data_X)
+
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.3, random_state=None)
+    for train_index, test_index in split.split(scaled_iris_data, iris_data_y):
+        train_x = scaled_iris_data[train_index]
+        train_y = iris_data_y[train_index]
+        test_x = scaled_iris_data[test_index]
+        test_y = iris_data_y[test_index]
 
     # Feature columns describe how to use the input.
     my_feature_columns = []
-    for key in train_x.keys():
+    train_x_keys = ['SepalLength', 'SepalWidth',
+                        'PetalLength', 'PetalWidth']
+    for key in train_x_keys:
         my_feature_columns.append(tf.feature_column.numeric_column(key=key))
 
     # Build 2 hidden layer DNN with 10, 10 units respectively.
@@ -47,32 +71,50 @@ def main(argv):
         # The model must choose between 3 classes.
         n_classes=3)
 
-    train_dict = dict(train_x)
+    train_x_shape = train_x.shape
+
+    # Method 1: convert train_x and train_y to pd. Series
+    '''
+        # Conver train_x to dict, and key value as Series
+        train_x_dict = dict()
+        for index, key in enumerate(train_x_keys):
+            train_x_dict[key] = Series(train_x[:, index])
+
+        train_y_shape = train_y.shape
+
+        train_y = Series(train_y.reshape(-1))
+    '''
+
+    # Method 2: keep train_x and train_y as array
+    train_x_dict = dict()
+    test_x_dict = dict()
+    for index, key in enumerate(train_x_keys):
+        train_x_dict[key] = train_x[:, index]
+        test_x_dict[key] = test_x[:, index]
 
     # Train the Model.
     classifier.train(
-        input_fn=lambda:iris_data.train_input_fn(train_dict, train_y,
+        input_fn=lambda:iris_data.train_input_fn_dict(train_x_dict, train_y,
                                                  args.batch_size),
         steps=args.train_steps)
 
     # Evaluate the model.
     eval_result = classifier.evaluate(
-        input_fn=lambda:iris_data.eval_input_fn(test_x, test_y,
+        input_fn=lambda:iris_data.eval_input_fn_dict(test_x_dict, test_y,
                                                 args.batch_size))
 
     print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
 
     # Generate predictions from the model
     expected = ['Setosa', 'Versicolor', 'Virginica']
-    predict_x = {
-        'SepalLength': [5.1, 5.9, 6.9],
-        'SepalWidth': [3.3, 3.0, 3.1],
-        'PetalLength': [1.7, 4.2, 5.4],
-        'PetalWidth': [0.5, 1.5, 2.1],
-    }
+    predict_x = np.array([[5.1, 3.3, 1.7, 0.5], [5.9, 3.0, 4.2, 1.5], [6.9, 3.1, 5.4, 2.1]])
+    scaled_predict_x = scaler.fit_transform(predict_x)
+    predict_x_dict = dict()
+    for index, key in enumerate(train_x_keys):
+        predict_x_dict[key] = scaled_predict_x[:, index]
 
     predictions = classifier.predict(
-        input_fn=lambda:iris_data.eval_input_fn(predict_x,
+        input_fn=lambda:iris_data.eval_input_fn_dict(predict_x_dict,
                                                 labels=None,
                                                 batch_size=args.batch_size))
 
